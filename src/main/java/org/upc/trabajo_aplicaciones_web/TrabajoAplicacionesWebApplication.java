80d0d77c01ee36ab23a763339e6c5b1a3fec3c7a
FabioV2006 <reynaldofabio3@gmail.com>
2025-09-29 19:34:55 -0500

    Primer commit: estructura base Spring Boot

---
+++ src/main/java/org/upc/trabajo_aplicaciones_web/TrabajoAplicacionesWebApplication.java
@@ -0,0 +1,13 @@
+package org.upc.trabajo_aplicaciones_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoAplicacionesWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrabajoAplicacionesWebApplication.class, args);
+    }
+
+}
