bd87e9b149bf7ab61168a32ef37f82bf1a57b465
Gdmania277% <matiasmoranalanya@gmail.com>
2025-10-05 20:38:15 -0500

    hora de duracion de token mejorada

--- src/main/java/org/upc/trabajo_aplicaciones_web/securities/JwtTokenUtil.java
+++ src/main/java/org/upc/trabajo_aplicaciones_web/securities/JwtTokenUtil.java
@@ -21,7 +21,8 @@ import java.util.stream.Collectors;
 public class JwtTokenUtil implements Serializable {
     private static final long serialVersionUID = -2550185165626007488L;
 
-
+    // 5 horas en milisegundos
+    public static final long JWT_TOKEN_VALIDITY = 7 * 60 * 60 * 1000;
 
     @Value("${jwt.secret}")
     private String secret;
